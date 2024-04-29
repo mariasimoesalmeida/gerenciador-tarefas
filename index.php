985715d5d6209001da936bf36ffd87657bd4ce92
mariasimoesalmeida <dudasimoesalmeida12@icloud.com>
2024-04-29 08:00:27 -0300

    criação para cadastrar as tarefas

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+    
+<div class="container">
+    <div class="header">
+        <h1>Gerenciador de Tarefas</h1>
+    </div>
+    <div class="form">
+        <form action="" method="get">
+            <label for="task_name"></label>
+             <input type="text" name="task_name" placeholder="Nome da Tarefa">
+             <button type="submit">Cadastrar</button>
+    </div>
+    <div class="separator">
+    </div>
+    <div class="list-tasks">
+    </div>
+    <div class="footer">
+   </div>
+</div>
+
+</body>
+</html>
